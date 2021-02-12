3b67ab400cdea02fd3a3f8b8b189a414f9648b49
Marysha <n01355798@humbermail.ca>
2021-02-12 16:07:43 -0500

    scroll buttons js added

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ closeMenuBtn.addEventListener("click", function () {
     html.classList.remove("noscroll");
   }
 });
+
+const btnLeft = document.querySelector("#left");
+const heroItems = document.querySelectorAll(".hero__articles__item");
+const heroItemsLastIndex = heroItems.length - 1;
+
+btnLeft.addEventListener("click", () => {
+  //set the item which is visible hidden and  previouse item visible
+  //if it is first item, make the last item visible
+  //  index = heroItems.values()
+  const index = Array.from(heroItems).findIndex(
+    (item) => !item.classList.contains("hidden")
+  );
+  const item = heroItems[index];
+  const prevItem =
+    index > 0 ? heroItems[index - 1] : heroItems[heroItemsLastIndex];
+  item.classList.add("hidden");
+  prevItem.classList.remove("hidden");
+});
+
+const btnRight = document.querySelector("#right");
+
+btnRight.addEventListener("click", () => {
+  const index = Array.from(heroItems).findIndex(
+    (item) => !item.classList.contains("hidden")
+  );
+  const item = heroItems[index];
+  const nextItem =
+    index < heroItemsLastIndex ? heroItems[index + 1] : heroItems[0];
+  item.classList.add("hidden");
+  nextItem.classList.remove("hidden");
+});
